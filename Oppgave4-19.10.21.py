2aaefebd55fa7a5029e9e56c8e095d39ac238f15
Ferdinand-Smedholen <ferdin1009@outlook.com>
2021-10-19 10:14:36 +0200

    m

--- Oppgave4-19.10.21.py
+++ Oppgave4-19.10.21.py
@@ -1,3 +1,3 @@
 ord=input("skriv inn ett ord: ") 
 bokstaver=len(ord)
-print(ord[0]+" "+ord[1]+" "+ord[bokstaver-2]+" "+ord[bokstaver-1])
+print(ord[0]+" "+ord[1]+"..."+ord[bokstaver-2]+" "+ord[bokstaver-1])
---
+++ oppgave5-19.10.21.py
@@ -0,0 +1,5 @@
+tall = int(input("Skriv inn et beløp i norske kroner: "))
+
+omgjøring = tall * 9.78
+
+print("Summen i Euro er: %0.2f" %omgjøring)
