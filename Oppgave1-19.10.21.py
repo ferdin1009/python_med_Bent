fa76ec72c46f07b483ff0f00b36b7afdb4257a25
Ferdinand-Smedholen <ferdin1009@outlook.com>
2021-10-19 09:20:30 +0200

    ez clap

--- Oppgave1-19.10.21.py
+++ Oppgave1-19.10.21.py
@@ -1,5 +1,5 @@
-tall=int (input("Skriv inn ett tall"))
-tall2=int (input("Skriv inn ett tall"))
+tall=int(input("Skriv inn ett tall: "))
+tall2=int(input("Skriv inn ett tall: "))
 
-svar= tall+tall2
-print("svaret er %d" &svar)
+svar= tall*tall2
+print("svaret er %d " %svar)
