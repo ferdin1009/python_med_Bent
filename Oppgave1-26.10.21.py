8fbef3b23401270b9c8c400b6ec5f905268ba284
Ferdinand-Smedholen <ferdin1009@outlook.com>
2021-10-26 09:54:02 +0200

    d

---
+++ Oppgave1-26.10.21.py
@@ -0,0 +1,7 @@
+størrelseTank=float(input("Hva er størrelsen på drivstofftanken din?"))
+bensinforbruk=float(input("Hva er drivstoff forbruket på bilen?"))
+bensinPris=float(input("Hva koster bensinen?"))
+lengde=float(input("Hvor langt skal du kjøre?"))
+
+
+
