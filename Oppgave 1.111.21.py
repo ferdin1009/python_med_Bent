55125f75ea49a0476e02d41dfb1e2e921323a229
Ferdinand-Smedholen <ferdin1009@outlook.com>
2021-11-02 13:15:55 +0100

    oppgave, bankkonto

---
+++ Oppgave 1.111.21.py
@@ -0,0 +1,10 @@
+sparek = float(input("skriv inn saldo på sparekonto: "))
+
+endring = input("vil du sette inn eller ta ut: ")
+if endring in ("sette inn"):
+    innskud = float(input("Hvor mye vil du sette inn "))
+    sparek = sparek + innskud
+else:
+    uttak = float(input("hvor mye vil du ta ut: "))
+    sparek = sparek - uttak
+print("ny saldo er"+ str(sparek) +"kroner" )
