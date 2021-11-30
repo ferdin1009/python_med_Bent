5baa26489446e4869c0344c052afc33c90d75b81
Ferdinand-Smedholen <ferdin1009@outlook.com>
2021-11-30 09:09:20 +0100

    ye

--- Oppgave-29.11.21.py
+++ Oppgave-29.11.21.py
@@ -1,11 +1,6 @@
-for i in range(5): #ytre løkke
-    print("rad",i,end=" : ")
-    for j in range(4): #indre løkke
-        print("x",end=" ")
-    print() #tom print-funksjon for å starte ny linje
+for i in range(1,11): 
+    for j in range(1,11):
+         print(i*j,end=" ")
+    print("\n")
 
-    #range-funskjon stopper FØR verdien tilstop-argumentet
-    for i in range(1,2): #Ytre for-løkke: bestemmer antall rader
-        for j in range(1,11): #Indre for-løkke: bestemmer antall kolonner
-            print(i*j, end=" ") #
     
